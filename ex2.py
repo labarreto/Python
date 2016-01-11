c89b5f0b79bcb43712e2382c1e34bcdb96bc44b1
Laura Barreto <labarreto@vassar.edu>
2016-01-11 00:06:17 -0600

    adding ex3

---
+++ ex2.py
@@ -0,0 +1,10 @@
+#Jan 11, 2016
+#Learning Python the Hard Way Exercise 2
+
+# a comment, this is so you can read your program later
+# anything after the # is ignored by Python
+
+print "I could have code like this." #with a comment after
+# print "this won't run"
+
+print "this will run."
---
+++ ex3.py
@@ -0,0 +1,26 @@
+#Jan 11, 2016
+#Learning Python the Hard Way Exercise 3
+
+
+# + plus
+# - minus
+# / divide
+# * multiply
+# % modulo
+# < less-than
+# > greater-than
+# <= less-than-equal
+# >= greater-than-equal
+
+print "I will now count my chickens:"
+
+print "Hens", 25 + 30 / 6
+#is python space sensitive with operations?
+print "Roosters", 100-25*3%4
+
+print "Now I will count the eggs:"
+print 3+2+1-5 +4 %2 - 1 /4 +6
+
+print "Is it true that 3 + 2 < 5 - 7"
+print 3 + 2 < 5 - 7
+
