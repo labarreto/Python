f7409c8ab3bf7d2d46a99737b67d73e29831b0f2
Laura Barreto <labarreto@vassar.edu>
2016-01-13 22:07:12 -0600

    adding 3, 4, 5

---
+++ ex4.py
@@ -0,0 +1,19 @@
+#Jan 13, 2016
+#Learning Python the Hard Way Exercise 4
+
+cars = 100
+space_in_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars-drivers
+cars_driven = drivers
+carpool_capacity = cars_driven *space_in_car
+average_passengers_per_car = passengers / cars_driven
+
+print "there are", cars, "cars available."
+print "there are only", drivers, "drivers available."
+print "there will be", cars-drivers, "empty cars today."
+print "we can transport", carpool_capacity, "people today."
+print "we have", passengers, "to carpool today."
+print "we need to put about", average_passengers_per_car, "in each car."
+
---
+++ ex5.py
@@ -0,0 +1,18 @@
+#Jan 13, 2016
+#Learning Python the Hard Way Exercise 5
+
+my_name = 'Laura Barreto'
+my_age = 20
+my_height = 62 #inches
+my_weight = 120 #lbs
+my_eyes = 'Brown'
+my_hair = 'Dark Brown'
+
+print "Let's talk about %s." % my_name
+print "She's %s inches tall." % my_height
+print "She's %s pounds heavy." % my_weight
+print "Actually that's not too heavy."
+print "She's got %s eyes and %s hair" % (my_eyes, my_hair)
+
+print "If I add %d, %d, and %d, I get %d." % (
+	my_age, my_height, my_weight, my_age + my_height + my_weight)
