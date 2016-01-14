8ba3747182b700341101938dea00bac239b82a3f
Laura Barreto <labarreto@vassar.edu>
2016-01-13 22:49:45 -0600

    changes to 5

--- ex5.py
+++ ex5.py
@@ -1,18 +1,22 @@
 #Jan 13, 2016
 #Learning Python the Hard Way Exercise 5
 
-my_name = 'Laura Barreto'
-my_age = 20
-my_height = 62 #inches
-my_weight = 120 #lbs
-my_eyes = 'Brown'
-my_hair = 'Dark Brown'
+name = 'Laura Barreto'
+age = 20
+height = 62 #inches
+weight = 120 #lbs
+eyes = 'Brown'
+hair = 'Dark Brown'
+# %d - format a number for display
+# %s - insert the presentation string
+# %r - insert canonical string
 
-print "Let's talk about %s." % my_name
-print "She's %s inches tall." % my_height
-print "She's %s pounds heavy." % my_weight
+
+print "Let's talk about %s." % name
+print "She's %s inches tall." % height
+print "She's %s pounds heavy." % weight
 print "Actually that's not too heavy."
-print "She's got %s eyes and %s hair" % (my_eyes, my_hair)
+print "She's got %s eyes and %s hair" % (eyes, hair)
 
 print "If I add %d, %d, and %d, I get %d." % (
-	my_age, my_height, my_weight, my_age + my_height + my_weight)
+	age, height, weight, age + height + weight)
