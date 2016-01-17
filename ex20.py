d48ceba2081a3fe6a42473bb71269ab06cf810a1
Laura Barreto <labarreto@vassar.edu>
2016-01-16 22:42:05 -0500

    updating files

---
+++ ex20.py
@@ -0,0 +1,32 @@
+#Jan 16, 2016
+#Learning Python the Hard Way Exercise 20
+
+from sys import argv
+
+script, input_file = argv
+
+def print_all(f):
+	print f.read()
+
+def rewind(f):
+	f.seek(0)
+
+def print_a_line(line_count, f):
+	print line_count, f.readline()
+
+current_file = open(input_file)
+
+print "print whole file: \n"
+print_all(current_file)
+print "let's rewind"
+rewind(current_file)
+
+
+current_line = 1
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
