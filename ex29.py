4f1c5e0ea4bdac3c3ff29fee357e1b8019d60621
Laura Barreto <labarreto@vassar.edu>
2016-02-24 22:42:13 -0500

    more files \!

---
+++ ex29.py
@@ -0,0 +1,28 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats:
+	#switched doomed to saved because c'mon, the world
+	#would be better with more cats
+	print "Too many cats! The world is saved!"
+
+if people > cats:
+	print "Not many cats! The world is doomed!"
+
+if people < dogs:
+	print "The world is drooled on by cool dogs"
+
+if people > dogs:
+	print "Not enough dogs in the world"
+
+dogs += 5
+
+if people >= dogs:
+	print "People are greater than or equal to dogs."
+
+if people <= dogs:
+	print "People are less than or equal to dogs."
+
+if people == dogs:
+	print "People are dogs."
